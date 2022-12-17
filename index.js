73206851bb5b9bd0096d2a86b3dbb28be0a4c51e
KiranPisal358 <120828972+KiranPisal358@users.noreply.github.com>
2022-12-18 01:19:54 +0530

    Add passenger counter files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+// initialize count as 0
+// listen for clicks on increment button
+// increment the count variable when the button is clicked
+// change the count-el in the html to reflect the count.
+
+
+let grMessage = document.getElementById("message");
+let name = "Kiran Pisal"
+let greeting = "Welcome back, "
+grMessage.textContent= greeting + name + "👋"
+
+let count = 0;
+let countEl = document.getElementById("count-el");
+function increment(){
+    count = count + 1;
+    countEl.textContent=count;
+}
+
+function decrement(){
+    count = count - 1;
+    countEl.textContent = count;
+}
+
+let saveEl = document.getElementById("save-el")
+function save(){
+    let saveCount = count + " - "
+    saveEl.textContent += saveCount;
+}
